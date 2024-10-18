4d16e70dafb6a80dc7737753a94def9370268cdc
Gulurin <122136008+Gulurin@users.noreply.github.com>
2024-10-18 11:27:42 +0900

    v1
    aaaaaaaaaaa

---
+++ 02.10.24/Manager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace _02._10._24
+{
+    class Manager
+    {
+        public static Frame MainFrame { get; set; }
+    }
+}
